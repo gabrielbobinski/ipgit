62cf6ad3cc2db66e5a01540b3cdcda868f543401
Gabriel Bobinski <gabrielbobinski@icloud.com>
2014-11-27 19:03:03 +0100

    test4

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -44,7 +44,7 @@ function peopleMenuClicked(){
 }
 function openProfilFoto(){
     $(".popupMiddle").load("/html/explore/profilFotoDetail.html");
-    //document.getElementById('popupMiddle').style.zIndex =20;
+    document.getElementById('popupMiddle').style.zIndex =20;
     //document.getElementById('back').style.opacity =0;
 }
 function peopleFotoClicked(){
