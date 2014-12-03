caa29b18a8f6adb56403121ce9d96f12b5e79e13
Gabriel Bobinski <gabrielbobinski@icloud.com>
2014-12-03 18:40:11 +0100

    3.12.14.7

--- public/javascripts/structure.js
+++ public/javascripts/structure.js
@@ -300,12 +300,9 @@ function drop(){
 };
 
 function disableBodyScroll(){
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     
-    }else
-    {
     document.getElementById('middelContent').style.overflow = 'hidden';
-    }
+    
 }
 
 function enableBodyScroll(){
